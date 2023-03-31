9e0d8d6a568f522162cad4ef6d1ea6fe07aa7d0e
Aneesh Dandime <aneesh.dandime9@gmail.com>
2023-03-31 18:04:53 -0400

    Finished chapter 2.

--- src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -9,5 +9,77 @@ object MyProgram:
     val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
 
+  private def formatFactorial(n: Int): String =
+    val msg = "The factorial of %d is %d"
+    msg.format(n, factorial(n))
+
+  def formatResult(name: String, n: Int, f: Int => Int): String =
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
+
+  def factorial(n: Int): Int =
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if n <= 0 then acc
+      else go(n - 1, n * acc)
+
+    go(n, 1)
+
+  def fib(n: Int): Int =
+    @annotation.tailrec
+    def loop(n: Int, a: Int, b: Int): Int =
+      if n <= 0 then a
+      else loop(n - 1, b, a + b)
+
+    loop(n, 0, 1)
+
   @main def printAbs: Unit =
     println(formatAbs(-42))
+    println(formatFactorial(7))
+    println(formatResult("absolute value", -10, abs))
+    println(fib(5))
+
+object Search:
+  def findFirst(ss: Array[String], key: String): Int =
+    @annotation.tailrec
+    def loop(n: Int): Int =
+      if n >= ss.length then -1
+      else if ss(n) == key then n
+      else loop(n + 1)
+
+    loop(0)
+
+object PolymorphicFunctions:
+  def findFirst[A](as: Array[A], p: A => Boolean): Int =
+    def loop(n: Int): Int =
+      if n >= as.length then -1
+      else if p(as(n)) then n
+      else loop(n + 1)
+
+    loop(0)
+
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean =
+    @annotation.tailrec
+    def loop(n: Int): Boolean =
+      if n >= as.length - 1 then true
+      else if gt(as(n), as(n + 1)) then false
+      else loop(n + 1)
+
+    loop(0)
+
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
+    (b: B) => f(a, b)
+
+  def curry[A, B, C](f: (A, B) => C): A => B => C =
+    (a: A) => (b: B) => f(a, b)
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    (a: A) => f(g(a))
+
+  @main def main(): Unit =
+    println(findFirst(Array(7, 9, 13), (x: Int) => x == 9))
+    println(isSorted(Array(1, 2, 3, 4, 5), _ > _))
+    println(isSorted(Array(1, 2, 3, 6, 5), _ > _))
